6985c46824502406b94494625290f86432c9bf37
РИС-24-3 Филатов Евгений <joutaroku@gmail.com>
2025-02-07 23:09:24 +0500

    Добавьте файлы проекта.

---
+++ Post.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory_9
+{
+    public class Post
+    {
+        private int numViews;
+        private int numComments;
+        private int numReactions;
+        private double engagementRate;
+        private static int postCount;
+
+        public int NumViews
+        {
+            get => numViews;
+            set 
+            {
+                if (value < 0)
+                    throw new Exception("Просмотры не могут быть отрицательными");
+                else
+                    numViews = value; 
+            }
+        }
+
+        public int NumComments
+        {
+            get => numComments;
+            set
+            {
+                if (value < 0)
+                    throw new Exception("Комментраии не могут быть отрицательными");
+                else
+                    numComments = value;
+            }
+        }
+        public int NumReactions
+        {
+            get => numReactions;
+            set
+            {
+                if (value < 0)
+                    throw new Exception("Реакции не могут быть отрицательными");
+                else
+                    numReactions = value;
+            }
+        }
+
+        public double EngagementRate
+        {
+            get => engagementRate;
+            set
+            {
+                if (value > 100)
+                    EngagementRate = 100;
+
+                else
+                    engagementRate = value;
+            }
+        }
+
+        public static int PostCount => postCount;
+
+        // Конструктор без параметров
+        public Post()
+        {
+            numViews = 0;
+            numComments = 0;
+            numReactions = 0;
+            postCount++;
+        }
+
+        // Конструктор с параметрами
+        public Post(int numViews, int numComments, int numReactions)
+        {
+            NumViews = numViews;
+            NumComments = numComments;
+            NumReactions = numReactions;
+            postCount++;
+        }
+
+        // Метод для вывода информации об объекте
+        public void PrintPostInfo()
+        {
+            Console.WriteLine($"Просмотры: {NumViews}, Комментарии: {NumComments}, Реакции: {NumReactions}");
+        }
+
+        public void CalculateEngagementRate(int numSubscribers)
+        {
+            if (numSubscribers <= 0) // Предотвращаем деление на ноль или отрицательное число подписчиков
+            {
+                EngagementRate = 0;
+                Console.WriteLine("Недопустимое количество подписчиков.");
+                return;
+            }
+
+            EngagementRate = ((double)(NumComments + NumReactions) / numSubscribers) * 100;
+            EngagementRate = Math.Round(EngagementRate, 2); // Округляем до 0,01
+        }
+
+        // Статическая функция для вычисления коэффициента вовлеченности
+        public static double CalculateEngagementRate(Post p, int numSubscribers)
+        {
+            if (numSubscribers <= 0) // Предотвращаем деление на ноль
+            {
+                p.EngagementRate = 0;
+                Console.WriteLine("Недопустимое количество подписчиков.");
+                return 0; // Или другое подходящее значение
+            }
+
+            p.EngagementRate = ((double)(p.NumComments + p.NumReactions) / numSubscribers) * 100;
+            p.EngagementRate = Math.Round(p.EngagementRate, 2); // Округляем до 0,01
+
+            return p.EngagementRate;
+        }
+
+        // Метод для вывода информации об вовлеченности объекта
+        public void PrintEngagementInfo()
+        {
+            Console.WriteLine($" {EngagementRate}");
+        }
+
+        // Перегрузка унарного оператора !
+        public static Post operator !(Post p)
+        {
+            p.NumReactions++;
+            return p;
+        }
+
+        // Перегрузка унарного оператора ++
+        public static Post operator ++(Post p)
+        {
+            p.NumViews++;
+            return p;
+        }
+
+        // Перегрузка оператора явного приведения к bool
+        public static explicit operator bool(Post p)
+        {
+            return p.NumViews > 0 && (p.NumComments > 0 || p.NumReactions > 0);
+        }
+
+        // Перегрузка оператора неявного приведения к double
+        public static implicit operator double(Post post)
+        {
+            const int subscribers = 1000;
+            return Math.Round((double)post.numViews / subscribers * 100, 2);
+        }
+
+        // Перегрузка оператора ==
+        public static bool operator ==(Post p1, Post p2)
+        {
+            //if (ReferenceEquals(p1, p2))
+            //    return true;
+
+            //if (ReferenceEquals(p1, null) || ReferenceEquals(p2, null))
+            //    return false;
+
+            return p1.numViews == p2.numViews &&
+                   p1.numComments == p2.numComments &&
+                   p1.numReactions == p2.numReactions;
+        }
+
+        // Перегрузка оператора !=
+        public static bool operator !=(Post p1, Post p2)
+        {
+            return !(p1 == p2);
+        }
+
+        // Метод Equals
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            Post other = (Post)obj;
+            return this == other;
+        }
+
+        //Переопределение метода GetHashCode(важно при переопределении Equals)
+        public override int GetHashCode()
+        {
+            int hash = 17;
+            hash = hash * 31 + NumViews.GetHashCode();
+            hash = hash * 31 + NumComments.GetHashCode();
+            hash = hash * 31 + NumReactions.GetHashCode();
+            return hash;
+
+            //return HashCode.Combine(numViews, numComments, numReactions);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Laboratory_9
+{
+internal class Program
+{
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Задание 1");
+                // Создание объекта класса Post
+                Post p1 = new Post();
+                Post p2 = new Post(1000, 52, 127);                
+               
+                Console.WriteLine("\nИнформация о первом посте:");
+                p1.PrintPostInfo();
+                Console.WriteLine("\nИнформация о втором посте:");
+                p2.PrintPostInfo();
+
+                // Вывод количества созданных объектов
+                Console.WriteLine($"\nКоличество созданных объектов Post: {Post.PostCount}");
+
+                // Вычисление и вывод коэффициента вовлеченности с помощью статической функции
+                Console.WriteLine("\nСтатическая функция:");
+                Post.CalculateEngagementRate(p1, 1000);
+                Console.Write("Коэффициент вовлеченности первого поста (проценты):");
+                p1.PrintEngagementInfo();
+
+                Post.CalculateEngagementRate(p2, 1000);
+                Console.Write("Коэффициент вовлеченности второго поста (проценты):");
+                p2.PrintEngagementInfo();
+
+                // Вычисление и вывод коэффициента вовлеченности с помощью метода объекта
+                Console.WriteLine("\nМетод объекта:");
+                p1.CalculateEngagementRate(1000);
+                Console.Write("Коэффициент вовлеченности первого поста (проценты):");
+                p1.PrintEngagementInfo();
+
+                p2.CalculateEngagementRate(1000);
+                Console.Write("Коэффициент вовлеченности второго поста (проценты):");
+                p2.PrintEngagementInfo();
+
+                Console.WriteLine("\nЗадание 2");
+                Post p3 = new Post(500, 50, 150);
+                Post p4 = new Post(500, 50, 150);
+                Post p5 = new Post(501, 50, 150);
+
+                // Унарные операции
+                p3 = !p3; // Увеличение количества реакций
+                ++p4; // Увеличение количества просмотров
+                Console.WriteLine("\nПосле унарных операций:");
+                p3.PrintPostInfo();
+                p4.PrintPostInfo();
+
+                // Операции приведения типа
+                bool hasEngagement = (bool)p3;
+                Console.WriteLine($"\nУ поста 3 есть вовлеченность: {hasEngagement}");
+
+                double reach = (double)p4;
+                Console.WriteLine($"\nОхват поста 4: {reach}%");
+
+                // Бинарные операции
+                Console.WriteLine("\nСравнение постов:");
+                Console.WriteLine($"p4 == p5: {p4 == p5}"); // Должно быть true
+                Console.WriteLine($"p3 != p4: {p3 != p4}"); // Должно быть true
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            finally
+            {
+                Console.WriteLine("\nКонец работы");
+            }
+        }
+    }
+}
+
